419cb0d4d90eb42e287219821da19c5f4fa72b29
RafaeNoor <abdurrafae98@live.com>
2024-04-18 13:42:00 -0500

    Pushing run script

---
+++ Baseline/run.sh
@@ -0,0 +1,6 @@
+
+# python3 test_model.py --modelPath model.csv --testDataPath data_test.csv
+
+#python3 test_model.py --modelPath model.csv --testDataPath data_test_small.csv
+
+python3 test_model.py --modelPath model.csv --testDataPath data_test.csv
